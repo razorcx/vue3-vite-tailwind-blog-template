504ebb03e4b44856f9e032e66dc460694e56bc2c
Cory LePage <cory@razorcx.com>
2023-08-01 11:00:11 -0700

    add views

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Header from "../components/Header.vue"
+import LatestArticles from "../components/LatestArticles.vue"
+import Podcasts from "../components/Podcasts.vue"
+import Cta from "../components/Cta.vue"
+import MostPopular from '../components/MostPopular.vue'
+import Footer from "../components/Footer.vue"
+import Nav from "../components/Nav.vue"
+</script>
+
+<template>
+    <div>
+        <Nav />
+
+        <Header />
+
+        <LatestArticles />
+
+        <Podcasts />
+
+        <Cta />
+
+        <MostPopular />
+
+        <Footer />
+    </div>
+</template>
