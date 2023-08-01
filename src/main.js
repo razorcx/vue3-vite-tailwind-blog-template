256c8a894ae0dcbe5abf8fde2806558bfdebc19c
Cory LePage <cory@razorcx.com>
2023-08-01 10:59:37 -0700

    add view router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './views/App.vue';
 import "./assets/app.css";
 
-createApp(App).mount('#app');
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name: 'Home', component: Home },
+        { path: '/about', name: 'About', component: About }
+    ]
+});
+
+createApp(App)
+    .use(router)
+    .mount('#app');
